e711eb0353f830821e016d18f71c7ced4b71caaa
Ammar-theScorpion <assiammar692@gmail.com>
2023-09-16 01:11:28 +0300

    design product table and simple test cases

--- ShopXen/Xen/models.py
+++ ShopXen/Xen/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField()
+    def __str__(self) -> str:
+        return self.name
--- ShopXen/Xen/urls.py
+++ ShopXen/Xen/urls.py
@@ -3,4 +3,5 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('checkout/', checkout, name='checkout'),
+    path('product/<str:pname>/', product, name='product'),
 ]
